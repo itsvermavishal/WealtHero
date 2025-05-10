8ab236b0ea61ae65892ca0ce29e0e10ab99f01bc
Vishal Verma <vishalpbh022@gmail.com>
2025-05-10 19:24:27 +0530

    changes and added more file

---
+++ app/src/main/java/com/example/wealthero/others/OthersTopper.java
@@ -0,0 +1,26 @@
+package com.example.wealthero.others;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.wealthero.R;
+
+public class OthersTopper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_others_topper);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
